9a7868f8db5e27548523a1584be686b6959e29f2
xWhiteePL <arturzaroda.pl@gmail.com>
2021-02-26 16:09:29 +0100

    12

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,6 @@
-module.exports = (client) => {
+module.exports = async (client) => {
 	console.log('Jestem gotowy!');
+	await client.user.setActivity("Funny Discord, / - token", {
+		type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+	  });
 };
